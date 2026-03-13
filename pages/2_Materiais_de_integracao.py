16b815104e61c48129f1d0cdcea0d8e5c9b59ba8
Bruna-gzr <brubsganzer@gmail.com>
2026-03-13 10:16:52 -0300

    Update 2_Materiais_de_integracao.py

--- pages/2_Materiais_de_integracao.py
+++ pages/2_Materiais_de_integracao.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="Materiais de Integração", layout="wide")
-import streamlit as st
+st.write("pagina materiais ok")
 
 # Configuração da página
 st.set_page_config(page_title="Materiais de Integração", layout="wide")
